60b181c7cb24e34601ef412d3a080427ad69392f
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2023-11-25 02:19:05 +0100

    Draft: Tentative fix for missing struct TypeInfo symbols
    Reverting #14844, which caused such missing TypeInfos, *and* making
    sure the special TypeInfo members are fully analyzed and ready for
    codegen (otherwise hitting an assertion).

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -7639,7 +7639,7 @@ public:
 
 extern Target target;
 
-extern Type* getTypeInfoType(const Loc& loc, Type* t, Scope* sc, bool genObjCode = true);
+extern Type* getTypeInfoType(const Loc& loc, Type* t, Scope* sc);
 
 class SemanticTimeTransitiveVisitor : public SemanticTimePermissiveVisitor
 {
